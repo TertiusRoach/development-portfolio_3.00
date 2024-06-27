7b67f0aa4fa0f5475b120caf212c0e7d8e40b773
Tertius Roach <tertius.roach@gmail.com>
2024-06-27 16:00:24 +0200

    Incorporated glow, now I can add the gradients and style it more approriately.

--- source/mods/button/glow/Button.glow.tsx
+++ source/mods/button/glow/Button.glow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Button.glow.scss';
 
 interface GlowProps {
@@ -6,11 +6,23 @@ interface GlowProps {
   className: string;
 }
 
-const ButtonGlow: React.FC<GlowProps> = ({ className, text }) => {
-  const glowingEffect = (element: React.MouseEvent<HTMLElement>): void => {};
+const ButtonGlow: React.FC<GlowProps> = ({ text, className }) => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const glowingEffect = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (buttonRef.current) {
+      const x = event.pageX - buttonRef.current.offsetLeft;
+      const y = event.pageY - buttonRef.current.offsetTop;
+
+      buttonRef.current.style.setProperty('--x', `${x}px`);
+      buttonRef.current.style.setProperty('--y', `${y}px`);
+    }
+  };
+
   return (
-    <button className={`${className} glow`}>
+    <button className={`${className} glow`} onMouseMove={glowingEffect} ref={buttonRef}>
       <h6>{text}</h6>
+      <div></div>
     </button>
   );
 };
