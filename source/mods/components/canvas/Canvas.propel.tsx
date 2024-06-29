48742ffd13c15a694b6ee6fa83b358a89f66fb49
Tertius Roach <tertius.roach@gmail.com>
2024-06-29 19:02:19 +0200

    You loaded the image into the <canva> tag! Now a whole world of possibilities exists.

--- source/mods/components/canvas/Canvas.propel.tsx
+++ source/mods/components/canvas/Canvas.propel.tsx
@@ -3,10 +3,6 @@ const REPEL_RADIUS = 50;
 const RETURN_SPEED = 0.1;
 const PARTICLE_DIAMETER = 2;
 
-const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-const particles: Particle[] = [];
-
 interface Particle {
   originX: number;
   originY: number;
@@ -15,92 +11,96 @@ interface Particle {
   axisX: number;
 }
 
-// const img = new Image();
-// img.src = '../profile-picture.png';
+const particles: Particle[] = [];
 
-// img.addEventListener('load', () => {
-//   canvas.width = img.width;
-//   canvas.height = img.height;
+export default function CanvasPropel(canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-//   ctx.drawImage(img, 0, 0);
+  const img = new Image();
+  img.src =
+    'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png';
 
-//   const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
 
-//   const numRows = Math.round(img.height / PARTICLE_DIAMETER);
-//   const numColumns = Math.round(img.width / PARTICLE_DIAMETER);
+    ctx.drawImage(img, 0, 0);
 
-//   for (let row = 0; row < numRows; row++) {
-//     for (let column = 0; column < numColumns; column++) {
-//       const pixelIndex = (row * PARTICLE_DIAMETER * img.width + column * PARTICLE_DIAMETER) * 4;
-//       const red = imageData[pixelIndex];
-//       const green = imageData[pixelIndex + 1];
-//       const blue = imageData[pixelIndex + 2];
-//       const alpha = imageData[pixelIndex + 3];
+    const imageData = ctx.getImageData(0, 0, img.width, img.height).data;
 
-//       particles.push({
-//         axisX: Math.floor(Math.random() * numColumns * PARTICLE_DIAMETER),
-//         axisY: Math.floor(Math.random() * numRows * PARTICLE_DIAMETER),
-//         originX: column * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
-//         originY: row * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
-//         color: `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`,
-//       });
-//     }
-//   }
-//   drawParticles();
-// });
+    const numRows = Math.round(img.height / PARTICLE_DIAMETER);
+    const numColumns = Math.round(img.width / PARTICLE_DIAMETER);
 
-// function drawParticles() {
-//   updateParticles();
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let row = 0; row < numRows; row++) {
+      for (let column = 0; column < numColumns; column++) {
+        const pixelIndex = (row * PARTICLE_DIAMETER * img.width + column * PARTICLE_DIAMETER) * 4;
+        const red = imageData[pixelIndex];
+        const green = imageData[pixelIndex + 1];
+        const blue = imageData[pixelIndex + 2];
+        const alpha = imageData[pixelIndex + 3];
 
-//   particles.forEach((particle) => {
-//     ctx.beginPath();
-//     ctx.arc(particle.axisX, particle.axisY, PARTICLE_DIAMETER / 2, 0, 2 * Math.PI);
-//     ctx.fillStyle = particle.color;
-//     ctx.fill();
-//   });
-//   requestAnimationFrame(drawParticles);
-// }
-
-let mouseX = Infinity;
-let mouseY = Infinity;
-
-canvas.addEventListener('mousemove', (event: MouseEvent) => {
-  mouseX = event.offsetX;
-  mouseY = event.offsetY;
-});
-
-canvas.addEventListener('mouseleave', () => {
-  mouseX = Infinity;
-  mouseY = Infinity;
-});
-
-export default function updateParticles() {
-  particles.forEach((particle) => {
-    const distanceFromMouseX = mouseX - particle.axisX;
-    const distanceFromMouseY = mouseY - particle.axisY;
-    const distanceFromMouse = Math.sqrt(distanceFromMouseX ** 2 + distanceFromMouseY ** 2);
-
-    if (distanceFromMouse < REPEL_RADIUS) {
-      const angle = Math.atan2(distanceFromMouseY, distanceFromMouseX);
-      const force = (REPEL_RADIUS - distanceFromMouse) / REPEL_RADIUS;
-
-      const moveX = Math.cos(angle) * force * REPEL_SPEED;
-      const moveY = Math.sin(angle) * force * REPEL_SPEED;
-
-      particle.axisX -= moveX;
-      particle.axisY -= moveY;
-    } else if (particle.axisX !== particle.originX || particle.axisY !== particle.originY) {
-      const distanceFromOriginX = particle.originX - particle.axisX;
-      const distanceFromOriginY = particle.originY - particle.axisY;
-      const distanceFromOrigin = Math.sqrt(distanceFromOriginX ** 2 + distanceFromOriginY ** 2);
-
-      const angle = Math.atan2(distanceFromOriginY, distanceFromOriginX);
-      const moveX = Math.cos(angle) * distanceFromOrigin * RETURN_SPEED;
-      const moveY = Math.sin(angle) * distanceFromOrigin * RETURN_SPEED;
-
-      particle.axisX += moveX;
-      particle.axisY += moveY;
+        particles.push({
+          axisX: Math.floor(Math.random() * numColumns * PARTICLE_DIAMETER),
+          axisY: Math.floor(Math.random() * numRows * PARTICLE_DIAMETER),
+          originX: column * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
+          originY: row * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
+          color: `rgba(${red}, ${green}, ${blue}, ${alpha / 255})`,
+        });
+      }
     }
+
+    drawParticles(canvas, ctx);
+  };
+
+  let mouseX = Infinity;
+  let mouseY = Infinity;
+
+  canvas.addEventListener('mousemove', (event: MouseEvent) => {
+    mouseX = event.offsetX;
+    mouseY = event.offsetY;
   });
+
+  canvas.addEventListener('mouseleave', () => {
+    mouseX = Infinity;
+    mouseY = Infinity;
+  });
+
+  function drawParticles(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach((particle) => {
+      ctx.beginPath();
+      ctx.arc(particle.axisX, particle.axisY, PARTICLE_DIAMETER / 2, 0, 2 * Math.PI);
+      ctx.fillStyle = particle.color;
+      ctx.fill();
+    });
+
+    applyRepelEffect();
+    requestAnimationFrame(() => drawParticles(canvas, ctx));
+  }
+
+  function applyRepelEffect() {
+    particles.forEach((particle) => {
+      const distanceFromMouseX = mouseX - particle.axisX;
+      const distanceFromMouseY = mouseY - particle.axisY;
+      const distanceFromMouse = Math.sqrt(distanceFromMouseX ** 2 + distanceFromMouseY ** 2);
+
+      if (distanceFromMouse < REPEL_RADIUS) {
+        const angle = Math.atan2(distanceFromMouseY, distanceFromMouseX);
+        const force = (REPEL_RADIUS - distanceFromMouse) / REPEL_RADIUS;
+
+        const moveX = Math.cos(angle) * force * REPEL_SPEED;
+        const moveY = Math.sin(angle) * force * REPEL_SPEED;
+
+        particle.axisX -= moveX;
+        particle.axisY -= moveY;
+      } else {
+        const distanceFromOriginX = particle.originX - particle.axisX;
+        const distanceFromOriginY = particle.originY - particle.axisY;
+
+        particle.axisX += distanceFromOriginX * RETURN_SPEED;
+        particle.axisY += distanceFromOriginY * RETURN_SPEED;
+      }
+    });
+  }
 }
--- source/mods/components/section/home/Section.home.tsx
+++ source/mods/components/section/home/Section.home.tsx
@@ -1,8 +1,9 @@
-// Section.home.tsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Section.home.scss';
 import { useMediaQuery } from 'react-responsive';
+
 import ButtonGlow from '../../button/glow/Button.glow';
+import initCanvas from '../../canvas/Canvas.propel';
 
 interface HomeProps {
   view: 'visible' | 'hidden';
@@ -16,172 +17,121 @@ interface HomeProps {
     portfolio: string;
     description: Array<string>;
   };
-  onMouseHover: (element: React.MouseEvent<HTMLElement>) => void; //--|🠈 Highlights the button of the viewed section on mouse interaction. 🠈|--//
-  onMouseClick: (trigger: React.MouseEvent<HTMLElement>, string: '<header>' | '<main>' | '<footer>') => void; //--|🠈 Centers the view on the user-clicked section. 🠈|--//
-
-  //--|🠊 Event: Reflects dynamic mouse interactions. 🠈|--//
-  //--|🠊 MouseEvent: Standard interface for DOM mouse events. 🠈|--//
-  //--|🠊 Void: Indicates that no value is returned by the toggle function. 🠈|--//
+  onMouseHover: (element: React.MouseEvent<HTMLElement>) => void;
+  onMouseClick: (trigger: React.MouseEvent<HTMLElement>, string: '<header>' | '<main>' | '<footer>') => void;
 }
 
 const SectionHome: React.FC<HomeProps> = ({
   className,
   view,
   tag: sectionObject,
-
   onMouseHover: activateButton,
   onMouseClick: scrollSection,
 }) => {
+  const canvasTag = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const canvas = canvasTag.current;
+    if (canvas) {
+      initCanvas(canvas);
+    } else {
+      console.log('Error')!;
+    }
+  }, []);
+
   return (
     <section
       className={`${className} ${view}`}
       onMouseEnter={activateButton}
       onClick={(trigger) => scrollSection(trigger, '<main>')}
     >
-      {
-        //--|🠋 Desktop (Landscape) 🠋|--//
-        useMediaQuery({ query: '(orientation: landscape) and (min-aspect-ratio: 16/9)' }) && (
-          <div className={`${className} desktop-landscape`}>
-            <span className="home-title">
-              <h1 data-text={sectionObject.title}>{sectionObject.title}</h1>
-            </span>
-            <span className="home-buttons">
-              <ButtonGlow className="career-button" text="My Career" />
-              <ButtonGlow className="contact-button" text="Contact Me" />
-            </span>
-            <span className="home-subject">
-              <h6>{sectionObject.subject}</h6>
-            </span>
+      {useMediaQuery({ query: '(orientation: landscape) and (min-aspect-ratio: 16/9)' }) && (
+        <div className={`${className} desktop-landscape`}>
+          <span className="home-title">
+            <h1 data-text={sectionObject.title}>{sectionObject.title}</h1>
+          </span>
+          <span className="home-buttons">
+            <ButtonGlow className="career-button" text="My Career" />
+            <ButtonGlow className="contact-button" text="Contact Me" />
+          </span>
+          <span className="home-subject">
+            <h6>{sectionObject.subject}</h6>
+          </span>
+          <span className="home-description">
+            <p>
+              {sectionObject.description[0]}
+              <br />
+              <br />
+              {sectionObject.description[1]}
+            </p>
+            <h6>{sectionObject.description[2]}</h6>
+          </span>
+          <canvas className="home-profile" ref={canvasTag}>
+            {/* <img
+              src="https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png"
+              alt="Tertius Roach"
+            /> */}
+          </canvas>
+        </div>
+      )}
+      {/*--|🠋 Desktop (Landscape) 🠋|--*/}
+      {useMediaQuery({ query: '(orientation: portrait) and (max-aspect-ratio: 1/1)' }) && (
+        <div className={`${className} mobile-portrait`}>
+          <span className="home-title">
+            <h3 data-text={sectionObject.title}>{sectionObject.title}</h3>
+          </span>
+          <aside className="home-profile">
+            <img
+              src="https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png"
+              alt="Tertius Roach"
+            />
+          </aside>
+          <span className="home-button">
+            <ButtonGlow className="career-button" text="My Career" />
+          </span>
+          <span className="home-subject">
+            <h6>{sectionObject.subject}</h6>
+          </span>
+          <span className="home-description">
             <span className="home-description">
-              <p>
-                {sectionObject.description[0]}
-                <br />
-                <br />
-                {sectionObject.description[1]}
-              </p>
-              <h6>{sectionObject.description[2]}</h6>
+              <p>{sectionObject.description[0]}</p>
             </span>
-
-            <aside className="home-profile">
-              <img
-                src="https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png"
-                alt="Tertius Roach"
-              />
-            </aside>
-          </div>
-        )
-      }
-      {
-        //--|🠋 Mobile (Portrait) 🠋|--//
-        useMediaQuery({ query: '(orientation: portrait) and (max-aspect-ratio: 1/1)' }) && (
-          <div className={`${className} mobile-portrait`}>
-            <span className="home-title">
-              <h3 data-text={sectionObject.title}>{sectionObject.title}</h3>
-            </span>
-            <aside className="home-profile">
-              <img
-                src="https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png"
-                alt="Tertius Roach"
-              />
-            </aside>
-            <span className="home-button">
-              <ButtonGlow className="career-button" text="My Career" />
-            </span>
-
-            <span className="home-subject">
-              <h6>{sectionObject.subject}</h6>
-            </span>
-            <span className="home-description">
-              <span className="home-description">
-                <p>{sectionObject.description[0]}</p>
-              </span>
-            </span>
-          </div>
-        )
-      }
-      {
-        //--|🠋 Tablet (Square) 🠋|--//
-        useMediaQuery({ query: '(max-aspect-ratio: 16/9) and (min-aspect-ratio: 1/1)' }) && (
-          <div className={`${className} tablet-square`}>
-            <span className="home-title">
-              <h1 data-text={sectionObject.title}>{sectionObject.title}</h1>
-            </span>
-            <span className="home-buttons">
-              <ButtonGlow className="career-button" text="My Career" />
-              <ButtonGlow className="contact-button" text="Contact Me" />
-            </span>
-            <span className="home-subject">
-              <h3>{sectionObject.subject}</h3>
-            </span>
-            <span className="home-description">
-              <p>
-                {sectionObject.description[0]}
-                <br />
-                <br />
-                {sectionObject.description[1]}
-              </p>
-              <h6>{sectionObject.description[2]}</h6>
-            </span>
-
-            <aside className="home-profile">
-              <img
-                src="https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png"
-                alt="Tertius Roach"
-              />
-            </aside>
-          </div>
-        )
-      }
+          </span>
+        </div>
+      )}
+      {/*--|🠋 Mobile (Portrait) 🠋|--*/}
+      {useMediaQuery({ query: '(max-aspect-ratio: 16/9) and (min-aspect-ratio: 1/1)' }) && (
+        <div className={`${className} tablet-square`}>
+          <span className="home-title">
+            <h1 data-text={sectionObject.title}>{sectionObject.title}</h1>
+          </span>
+          <span className="home-buttons">
+            <ButtonGlow className="career-button" text="My Career" />
+            <ButtonGlow className="contact-button" text="Contact Me" />
+          </span>
+          <span className="home-subject">
+            <h3>{sectionObject.subject}</h3>
+          </span>
+          <span className="home-description">
+            <p>
+              {sectionObject.description[0]}
+              <br />
+              <br />
+              {sectionObject.description[1]}
+            </p>
+            <h6>{sectionObject.description[2]}</h6>
+          </span>
+          <aside className="home-profile">
+            <img
+              src="https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00/main/public/content/png-files/profile-picture.png"
+              alt="Tertius Roach"
+            />
+          </aside>
+        </div>
+      )}
+      {/*--|🠋 Tablet (Square) 🠋|--*/}
     </section>
   );
-
-  const desktopLandscape: string = '(orientation: landscape) and (min-aspect-ratio: 16/9)';
-  const mobilePortrait: string = '(orientation: portrait) and (max-aspect-ratio: 1/1)';
-  const tabletSquare: string = '(max-aspect-ratio: 16/9) and (min-aspect-ratio: 1/1)';
-
-  console.log(desktopLandscape);
-  console.log(mobilePortrait);
-  console.log(tabletSquare);
 };
+
 export default SectionHome;
-
-// Archive-it
-let projectURI: string = 'https://raw.githubusercontent.com/TertiusRoach/development-portfolio_3.00';
-interface Designs {
-  logo: string;
-  home: string;
-  email: string;
-  skills: string;
-  projects: string;
-  telephone: string;
-  cellphone: string;
-}
-export let buttonIcon: Designs = {
-  logo: `${projectURI}/17d708556905ed2a95bf3329e9dd6411792b16b9/public/content/svg-files/signature-adjacent/primary-light.svg`,
-  home: `${projectURI}/33ecc4eebaa902dbc040024fbc24833dd4e1fdd1/public/content/svg-files/icon-collection/duotone/home.svg`,
-  email: `${projectURI}/b6fafac72b6b8c324b14d8a0560ad55e2e803096/public/content/svg-files/icon-collection/duotone/envelope-open-text.svg`,
-  skills: `${projectURI}/33ecc4eebaa902dbc040024fbc24833dd4e1fdd1/public/content/svg-files/icon-collection/duotone/lightbulb-on.svg`,
-  projects: `${projectURI}/64766ee2eec5d723042a565c96644dd5d3b5732d/public/content/svg-files/icon-collection/duotone/code.svg`,
-  telephone: `${projectURI}/33ecc4eebaa902dbc040024fbc24833dd4e1fdd1/public/content/svg-files/icon-collection/duotone/phone-volume.svg`,
-  cellphone: `${projectURI}/b6fafac72b6b8c324b14d8a0560ad55e2e803096/public/content/svg-files/icon-collection/duotone/mobile-android-alt.svg`,
-};
-
-interface Websites {
-  github: Array<string>;
-  youtube: Array<string>;
-  linkedin: Array<string>;
-}
-export let anchorIcon: Websites = {
-  linkedin: [
-    `${projectURI}/8581795ce870e5465e4673403a22c14f5b9c156f/public/content/svg-files/icon-collection/brands/linkedin.svg`,
-    'https://www.linkedin.com/in/tertius-roach/',
-  ],
-  github: [
-    `${projectURI}/8581795ce870e5465e4673403a22c14f5b9c156f/public/content/svg-files/icon-collection/brands/github.svg`,
-    'https://github.com/TertiusRoach',
-  ],
-  youtube: [
-    `${projectURI}/8581795ce870e5465e4673403a22c14f5b9c156f/public/content/svg-files/icon-collection/brands/youtube.svg`,
-    'https://www.youtube.com/@TertiusRoach',
-  ],
-};
